8ad661a0478e47b2b21359e14727ccf186a41b23
Ryan McNair <ryan.mcnair@gmail.com>
2020-11-20 18:38:51 -0600

    Added forms, functions and hooked up CREATE in big swoop

---
+++ src/components/Forms.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Button, Form, Input } from 'reactstrap';
+
+export default class PlayerForm extends Component {
+  state = {
+    id: this.props.player.id || '',
+    name: this.props.player.name || '',
+    imgUrl: this.props.player.imageUrl || '',
+    number: this.props.player.number || '',
+  }
+
+  handleChange = (e) => {
+    const value = e.target.name === 'number' ? Number(e.target.value) : e.target.value;
+    this.setState({
+      [e.target.name]: value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addUpdatePlayer(this.state);
+  };
+
+  render() {
+    const dataItems = ['name', 'imageUrl', 'position'];
+    return (
+      <Form className='container mb-3' onSubmit={this.handleSubmit}>
+            {
+                dataItems.map((item) => (
+                  (item !== 'id') && (
+                   <Input
+                    key={item}
+                    className='mb-2'
+                    type='text'
+                    name={item}
+                    id={item}
+                    value={this.state[`${item}`]}
+                    placeholder={`Enter player ${item}`}
+                    onChange={this.handleChange}
+                    required
+                   />)
+                ))
+            }
+            <Button className="mt-3">Submit</Button>
+        </Form>
+    );
+  }
+}
--- src/helpers/data/playerData.js
+++ src/helpers/data/playerData.js
@@ -8,4 +8,23 @@ const getPlayers = () => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
-export default { getPlayers };
+const addPlayer = (playerObj) => new Promise((resolve, reject) => {
+  axios.post(`${baseUrl}.json`, playerObj).then((resp) => {
+    axios.patch(`${baseUrl}/${resp.data.name}.json`, { id: resp.data.name }).then((patchResp) => {
+      resolve(patchResp);
+    }).catch((error) => reject(error));
+  });
+});
+
+const updatePlayer = (playerObj) => new Promise((resolve, reject) => {
+  axios.patch(`${baseUrl}/${playerObj.id}.json`, playerObj).then((resp) => {
+    resolve(resp);
+  }).catch((error) => reject(error));
+});
+
+const deletePlayer = (playerId) => axios.delete(`${baseUrl}/${playerId}.json`);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getPlayers, addPlayer, updatePlayer, deletePlayer,
+};
